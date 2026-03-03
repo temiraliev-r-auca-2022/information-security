37d50dafc2389dfd831cb15ded2aefe4923e0063
temiraliev-r-auca-2022 <temiraliev_r@auca.kg>
2026-03-03 07:18:59 -0500

    lab10

---
+++ lab-10/api.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from API server")
+
+server = HTTPServer(("0.0.0.0", 5000), Handler)
+server.serve_forever()
